32a78d53364c8ac1325d74d8a54cbaacd80a0a30
Balsam-Faysal <blsam.2332016@gmail.com>
2018-05-22 11:26:10 +0300

    #7 finish style of home page and add dom

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -7,32 +7,42 @@ const getWeekMentors = require('../model/quires/get_week_mentors');
 const getWeekworkshops = require('../model/quires/get_workshops');
 
 
+const getRightFormatDate = () => {
+  const dateObj = new Date();
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const dayName = days[dateObj.getDay()];
+  const month = dateObj.getUTCMonth() + 1; // months from 1-12
+  const day = dateObj.getUTCDate();
+  const year = dateObj.getUTCFullYear();
+  const newdate = `${year}/${month}/${day}, ${dayName}`;
+  return newdate;
+};
+
+
 exports.get = (req, res) => {
-  getDate((getDateErr, getDateResult) => {
-    if (getDateErr) return res.status(500);
-    getPresentStudent((getPresentStudentErr, getPresentStudentResult) => {
-      if (getPresentStudentErr) return res.status(500);
-      getAbsentStudent((getAbsentStudentErr, getAbsentStudentResult) => {
-        if (getAbsentStudentErr) return res.status(500);
-        getLateStudent((getLateStudentErr, getLateStudentResult) => {
-          if (getLateStudentErr) return res.status(500);
-          getLeaveStudent((getLeaveStudentErr, getLeaveStudentResult) => {
-            if (getLeaveStudentErr) return res.status(500);
-            getWeekMentors((getWeekMentorsErr, getWeekMentorsResult) => {
-              if (getWeekMentorsErr) return res.status(500);
-              getWeekworkshops((getWeekworkshopsErr, getWeekworkshopsResult) => {
-                if (getWeekworkshopsErr) return res.status(500);
-                return res.render('home', {
-                  date: getDateResult[0],
-                  presentSts: getPresentStudentResult,
-                  absentSts: getAbsentStudentResult,
-                  lateSts: getLateStudentResult,
-                  leaveSts: getLeaveStudentResult,
-                  weekMentors: getWeekMentorsResult[0],
-                  weekWorkshops: getWeekworkshopsResult,
-                  weekWorkshops1: getWeekworkshopsResult[0],
-                  style_home: 'home_style.css',
-                });
+  getPresentStudent((getPresentStudentErr, getPresentStudentResult) => {
+    if (getPresentStudentErr) return res.status(500);
+    getAbsentStudent((getAbsentStudentErr, getAbsentStudentResult) => {
+      if (getAbsentStudentErr) return res.status(500);
+      getLateStudent((getLateStudentErr, getLateStudentResult) => {
+        if (getLateStudentErr) return res.status(500);
+        getLeaveStudent((getLeaveStudentErr, getLeaveStudentResult) => {
+          if (getLeaveStudentErr) return res.status(500);
+          getWeekMentors((getWeekMentorsErr, getWeekMentorsResult) => {
+            if (getWeekMentorsErr) return res.status(500);
+            getWeekworkshops((getWeekworkshopsErr, getWeekworkshopsResult) => {
+              if (getWeekworkshopsErr) return res.status(500);
+              return res.render('home', {
+                my_date: getRightFormatDate(),
+                presentSts: getPresentStudentResult,
+                absentSts: getAbsentStudentResult,
+                lateSts: getLateStudentResult,
+                leaveSts: getLeaveStudentResult,
+                weekMentors: getWeekMentorsResult,
+                weekWorkshops: getWeekworkshopsResult,
+                weekWorkshops1: getWeekworkshopsResult[0],
+                style_home: 'home_style.css',
+                script: 'home_dom.js',
               });
             });
           });
