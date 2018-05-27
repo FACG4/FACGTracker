54a2f784de37cc3f96516213056258c2871cace4
amusamhe <abdalsamad.y.m@gmail.com>
2018-05-27 06:05:45 +0300

    split the to trackBox into a partial and rearrange the code

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,40 +1,32 @@
-const getPresentStudent = require('../model/quires/get_present_students');
-const getAbsentStudent = require('../model/quires/get_absent_students');
-const getLateStudent = require('../model/quires/get_late_students');
-const getLeaveStudent = require('../model/quires/get_leave_students');
+const formatDate = require('./format_date');
 const getWeekMentors = require('../model/quires/get_week_mentors');
 const getWeekworkshops = require('../model/quires/get_workshops');
-const formatDate = require('./format_date');
+const trackBoxQueries = require('../model/quires/track_box_queries');
 
+const date = formatDate.getRightFormatDate().newdate.split(',')[1];
 
 exports.get = (req, res) => {
-  getPresentStudent((getPresentStudentErr, getPresentStudentResult) => {
-    if (getPresentStudentErr) return res.status(500);
-    getAbsentStudent((getAbsentStudentErr, getAbsentStudentResult) => {
-      if (getAbsentStudentErr) return res.status(500);
-      getLateStudent((getLateStudentErr, getLateStudentResult) => {
-        if (getLateStudentErr) return res.status(500);
-        getLeaveStudent((getLeaveStudentErr, getLeaveStudentResult) => {
-          if (getLeaveStudentErr) return res.status(500);
-          getWeekMentors((getWeekMentorsErr, getWeekMentorsResult) => {
-            if (getWeekMentorsErr) return res.status(500);
-            getWeekworkshops((getWeekworkshopsErr, getWeekworkshopsResult) => {
-              if (getWeekworkshopsErr) return res.status(500);
-              return res.render('home', {
-                my_date: formatDate.getRightFormatDate().newdate,
-                presentSts: getPresentStudentResult,
-                absentSts: getAbsentStudentResult,
-                lateSts: getLateStudentResult,
-                leaveSts: getLeaveStudentResult,
-                weekMentors: getWeekMentorsResult,
-                weekWorkshops: getWeekworkshopsResult,
-                weekWorkshops1: getWeekworkshopsResult[0],
-                style: ['home_style.css'],
-                script: ['home_dom.js'],
-              });
-            });
+  getWeekMentors((getWeekMentorsErr, getWeekMentorsResult) => {
+    if (getWeekMentorsErr) return res.status(500);
+    getWeekworkshops((getWeekworkshopsErr, getWeekworkshopsResult) => {
+      if (getWeekworkshopsErr) return res.status(500);
+      trackBoxQueries.getTrackBoxInfo(date, (err, result) => {
+        if (err) {
+          console.log('TrackboxInfoErr', err);
+        } else {
+          res.render('home', {
+            my_date: formatDate.getRightFormatDate().newdate,
+            presentSts: result.presentSts,
+            absentSts: result.absentSts,
+            lateSts: result.lateSts,
+            leaveSts: result.leaveSts,
+            weekMentors: getWeekMentorsResult,
+            weekWorkshops: getWeekworkshopsResult,
+            weekWorkshops1: getWeekworkshopsResult[0],
+            style: ['home_style.css'],
+            script: ['home_dom.js'],
           });
-        });
+        }
       });
     });
   });
